abb2582bad63acec0347b70901fbc3b4488955b4
Chenentano <Zarata123@yahoo.de>
2023-11-01 14:34:38 +0100

    Footer -> Navbar oben rechts

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -1,8 +1,18 @@
+"use client";
+
+import { useState } from "react";
 import { Socials } from "@/constants";
 import Image from "next/image";
 import React from "react";
+import { RxDiscordLogo, RxGithubLogo, RxLinkedinLogo } from "react-icons/rx";
 
 const Navbar = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
   return (
     <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
       <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
@@ -37,16 +47,56 @@ const Navbar = () => {
           </div>
         </div>
 
-        <div className="flex flex-row gap-5">
-          {Socials.map((social) => (
-            <Image
-              src={social.src}
-              alt={social.name}
-              key={social.name}
-              width={24}
-              height={24}
-            />
-          ))}
+        <div className="relative">
+          <button
+            onClick={toggleDropdown}
+            className="flex flex-row items-center gap-2 cursor-pointer"
+          >
+            <span className="text-gray-200">GitHub und Social</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className={`h-6 w-6 text-gray-200 transform transition-transform ${
+                isDropdownOpen ? "-rotate-180" : "rotate-0"
+              }`}
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M19 9l-7 7-7-7"
+              />
+            </svg>
+          </button>
+          {isDropdownOpen && (
+            <div className="absolute top-8 right-0 bg-[#0300145e] border border-[#7042f861] py-2 px-4 rounded-lg text-gray-200">
+              <a
+                href="https://github.com/chenentano"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="block hover:text-blue-400"
+              >
+                <div className="flex flex-row items-center my-[15px] cursor-pointer">
+                  <RxGithubLogo style={{ fontSize: "24px" }} />
+                  <span className="text-[15px] ml-[6px]">GitHub</span>
+                </div>
+              </a>
+
+              <a
+                href="https://www.linkedin.com/in/bastian-behrendt-92904b1b4/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="block hover:text-blue-400"
+              >
+                <div className="flex flex-row items-center my-[15px] cursor-pointer">
+                  <RxLinkedinLogo style={{ fontSize: "24px" }} />
+                  <span className="text-[15px] ml-[6px]">LinkedIn</span>
+                </div>
+              </a>
+            </div>
+          )}
         </div>
       </div>
     </div>
